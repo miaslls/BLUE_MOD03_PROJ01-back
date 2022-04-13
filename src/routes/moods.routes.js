79500ec3cdba337a5d9810b559006e1acdc54e7a
miaslls <97785569+miaslls@users.noreply.github.com>
2022-04-13 12:13:39 -0300

    fix(routes): NOW it's correct

--- src/routes/moods.routes.js
+++ src/routes/moods.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const moodsController = require('../controllers/moods.controller');
 
-router.get('mood/all', moodsController.findMoodsController);
-router.get('mood/:createdat', moodsController.findMoodByCreatedatController);
+router.get('/all', moodsController.findMoodsController);
+router.get('/:createdat', moodsController.findMoodByCreatedatController);
 
 module.exports = router;
