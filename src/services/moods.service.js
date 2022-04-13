c294c428961dc825b895b3c5724b5d95bb4d7b4f
miaslls <97785569+miaslls@users.noreply.github.com>
2022-04-13 10:32:43 -0300

    feat(service): structured in accordance to AULA03

---
+++ src/services/moods.service.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const moods = [
+  {
+    mood_id: 0,
+    icon: '',
+    text: 'text mood 0',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+  {
+    mood_id: 1,
+    icon: '',
+    text: 'text mood 1',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+  {
+    mood_id: 2,
+    icon: '',
+    text: 'text mood 2',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+  {
+    mood_id: 3,
+    icon: '',
+    text: 'text mood 3',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+  {
+    mood_id: 4,
+    icon: '',
+    text: 'text mood 4',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+  {
+    mood_id: 5,
+    icon: '',
+    text: 'text mood 5',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+  {
+    mood_id: 6,
+    icon: '',
+    text: 'text mood 6',
+    date: '0000-00-00',
+    time: 'T00:00:00',
+    timestamp: 00000000000000,
+    createdat: '0000-00-00T00:00:00',
+  },
+];
+
+const findMoodsService = () => {
+  return moods;
+};
+
+const findPaletaByCreatedatService = (createdat) => {
+  return moods.find((mood) => mood.createdat === createdat);
+};
+
+const findPaletaByDateService = (date) => {
+  return moods.find((mood) => mood.date === date);
+};
+
+module.exports = {
+  findMoodsService,
+  findPaletaByCreatedatService,
+  findPaletaByDateService,
+};
