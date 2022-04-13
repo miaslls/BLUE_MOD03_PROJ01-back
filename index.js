7fe3dc80ff18077851a1ae731c3d4ef1628d15d4
miaslls <97785569+miaslls@users.noreply.github.com>
2022-04-13 11:13:15 -0300

    fix(?): feels better. correct? yime will tell

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/mood', routes);
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port} 🔗`);
