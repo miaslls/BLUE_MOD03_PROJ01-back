b6a30612e2fa42236983d0d7957b0db3aac5a07d
miaslls <97785569+miaslls@users.noreply.github.com>
2022-04-13 10:59:49 -0300

    feat(controller): struct. in accordance to AULA03

---
+++ src/controllers/moods.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const moodsService = require('../services/moods.service');
+
+const findMoodsController = (req, res) => {
+  const allMoods = moodsService.findMoodsService();
+  res.send(allMoods);
+};
+
+const findMoodByCreatedatController = (req, res) => {
+  const createdatParam = req.params.createdat;
+  const chosenMood = moodsService.findMoodByCreatedatService(createdatParam);
+  res.send(chosenMood);
+};
+
+const findMoodByDateController = (req, res) => {
+  const dateParam = req.params.date;
+  const chosenMood = moodsService.findMoodByDateService(dateParam);
+  res.send(chosenMood);
+};
+
+module.exports = {
+  findMoodsController,
+  findMoodByCreatedatController,
+  findMoodByDateController,
+};
