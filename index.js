65bf2ea022aca543b535b58c289b7395c96db836
miaslls <97785569+miaslls@users.noreply.github.com>
2022-04-14 21:19:53 -0300

    chore(deploy): Procfile + port config

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const routes = require('./src/routes/moods.routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
